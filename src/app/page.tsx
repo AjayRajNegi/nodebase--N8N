135599e7059ac25df95f477c41c4b8bcc18e6790
Ajay <ajayrajnegi111@gmail.com>
2025-10-17 13:35:31 +0530

    01: Authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
+import { LogoutButton } from "./Logout";
 
 export default async function Page() {
   await requireAuth();
-  return <div>asd</div>;
+  const data = await caller.getUsers();
+  return (
+    <div>
+      {JSON.stringify(data)} <LogoutButton />
+    </div>
+  );
 }
