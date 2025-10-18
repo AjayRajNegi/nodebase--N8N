0d8531c09a709cc99e4750b27070c31e7712b67f
Ajay <ajayrajnegi111@gmail.com>
2025-10-18 10:18:33 +0530

    03: Background Jobs - Added inngest to run multiple dev processes concurrently.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,32 @@
-import { requireAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
-import { LogoutButton } from "./Logout";
+"use client";
 
-export default async function Page() {
-  await requireAuth();
-  const data = await caller.getUsers();
+import { LogoutButton } from "./Logout";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
+export default function Page() {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        toast.success("Job queued.");
+      },
+    })
+  );
+
   return (
-    <div>
-      {JSON.stringify(data)} <LogoutButton />
-    </div>
+    <>
+      <div>
+        {JSON.stringify(data)} <LogoutButton />
+      </div>
+      <Button onClick={() => create.mutate()} disabled={create.isPending}>
+        Create WOrkflow
+      </Button>
+    </>
   );
 }
