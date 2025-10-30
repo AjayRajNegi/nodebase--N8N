eba8d7a89c66e14b7225285fcb30bd073ef9192e
Ajay <ajayrajnegi111@gmail.com>
2025-10-30 16:13:08 +0530

    Workflow search added

--- src/app/(dashboard)/(rest)/layout.tsx
+++ src/app/(dashboard)/(rest)/layout.tsx
@@ -3,8 +3,8 @@ import AppHeader from "@/components/app-header";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {/* <AppHeader />
-      <main className="flex-1">{children}</main> */}
+      <AppHeader />
+      <main className="flex-1">{children}</main>
     </>
   );
 }
