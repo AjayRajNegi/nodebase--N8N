0dc959c503378d433e1e0fa70ae9d277db59dc4e
Ajay <ajayrajnegi111@gmail.com>
2025-11-12 16:56:34 +0530

    17: Editor State

--- src/app/features/executions/components/http-request/node.tsx
+++ src/app/features/executions/components/http-request/node.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { memo } from "react";
-import { Node, NodeProps } from "@xyflow/react";
+import { memo, useState } from "react";
+import { Node, NodeProps, useReactFlow } from "@xyflow/react";
 import { BaseExecutionNode } from "../base-execution-node";
 import { GlobeIcon } from "lucide-react";
+import { FormType, HTTPRequestDialog } from "./dialog";
 
 type HttpRequestNodeData = {
   endpoint?: string;
@@ -15,15 +16,48 @@ type HttpRequestNodeData = {
 type HttpRequestNodeType = Node<HttpRequestNodeData>;
 
 export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const { setNodes } = useReactFlow();
+
+  const nodeStatus = "initial";
+
+  const handleOpenSettings = () => {
+    setDialogOpen(true);
+  };
+  const handleSubmt = (values: FormType) => {
+    setNodes((nodes) =>
+      nodes.map((node) => {
+        if (node.id === props.id) {
+          return {
+            ...node,
+            data: {
+              ...node.data,
+              endpoint: values.endpoint,
+              method: values.method,
+              body: values.body,
+            },
+          };
+        }
+        return node;
+      })
+    );
+  };
+
   const nodeData = props.data;
   const description = nodeData?.endpoint
     ? `${nodeData.method || "HET"}: ${nodeData.endpoint}`
     : "Not configured";
 
-  const nodeStatus = "initial";
-
   return (
     <>
+      <HTTPRequestDialog
+        open={dialogOpen}
+        onOpenChange={setDialogOpen}
+        onSubmit={handleSubmt}
+        defaultEndpoint={nodeData.endpoint}
+        defaultMethod={nodeData.method}
+        defaultBody={nodeData.body}
+      />
       <BaseExecutionNode
         {...props}
         id={props.id}
@@ -31,8 +65,8 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
         name="HTTP Request"
         status={nodeStatus}
         description={description}
-        onSettings={() => {}}
-        onDoubleClick={() => {}}
+        onSettings={handleOpenSettings}
+        onDoubleClick={handleOpenSettings}
       />
     </>
   );
